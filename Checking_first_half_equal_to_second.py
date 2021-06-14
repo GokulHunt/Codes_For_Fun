563f54e9c12be86c87a2f9614beab3d6cf7dfede
GokulHunt <gokulavarmanseshadri@gmail.com>
2021-06-14 09:34:26 +0530

    Checking sums of first half and second half
    for input with odd lengths middle number will be excluded

---
+++ Checking_first_half_equal_to_second.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 09:11:21 2021
+
+@author: gokul
+"""
+
+def check_lhs_rhs(ip):
+    data = []
+    for i in ip:
+        data.append(int(i))
+    
+    if len(data)%2 == 1:
+        if sum(data[:(len(data))//2]) == sum(data[(len(data)+1)//2 :]):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if sum(data[:(len(data))//2]) == sum(data[(len(data))//2 :]):
+            print("YES")
+        else:
+            print("NO")
+    
+check_lhs_rhs('1222006')
+    
